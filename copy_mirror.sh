b5b00555005d9065f67de59d448538a44132c792
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2021-03-23 11:51:57 +0100

    Move copy to a script, to avoid quoting

---
+++ copy_mirror.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+id=$(docker create ghcr.io/alalazo/manylinux1_mirrors_x86_64:latest)
+docker pull ghcr.io/alalazo/manylinux1_mirrors_x86_64:latest
+docker cp $id:/home/spack/binary-mirror binary-mirror
+docker rm -v $id
