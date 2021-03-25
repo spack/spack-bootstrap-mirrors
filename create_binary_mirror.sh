572e5b2f64bf5fc6c6c29fe50c8bc6a855222216
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2021-03-25 14:58:37 +0100

    Don't create mirrors with relative RPATHs

--- create_binary_mirror.sh
+++ create_binary_mirror.sh
@@ -2,4 +2,4 @@
 
 specs=$(${SPACK_CMD} find --format="/{hash}" clingo-bootstrap)
 mkdir -p /home/spack/binary-mirror
-${SPACK_CMD} buildcache create -d /home/spack/binary-mirror -a -u -f -r ${specs}
+${SPACK_CMD} buildcache create -d /home/spack/binary-mirror -a -u -f ${specs}
