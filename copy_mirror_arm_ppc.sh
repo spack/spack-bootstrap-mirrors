3ff739665b51eb7800d2c03fb024326ce4b0d642
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2021-04-13 10:26:17 +0200

    Skip building manylinux2014 images, try to upload mirrors

--- copy_mirror_arm_ppc.sh
+++ copy_mirror_arm_ppc.sh
@@ -6,6 +6,6 @@ docker cp $id:/home/spack/binary-mirror arm64/binary-mirror
 docker rm -v $id
 
 id=$(docker create --platform linux/ppc64le ghcr.io/alalazo/manylinux2014_mirrors:latest)
-mkdir ppc64le64
+mkdir ppc64le
 docker cp $id:/home/spack/binary-mirror ppc64le/binary-mirror
 docker rm -v $id
