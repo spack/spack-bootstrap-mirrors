a09320adf8e24d9da09aa7418164906aa8b08409
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2021-06-17 14:19:49 +0200

    Clean-up code for generating clingo on arm64 and ppc64le (#7)

--- create_binary_mirror.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-specs=$(${SPACK_CMD} find --format="/{hash}" clingo-bootstrap)
-mkdir -p /home/spack/binary-mirror
-${SPACK_CMD} buildcache create -d /home/spack/binary-mirror -a -u -f ${specs}
-
