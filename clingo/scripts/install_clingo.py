f0ee8ce70cfa97f57f44e71abd3908e3931aae53
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2023-10-09 10:27:33 +0200

    Simplify clingo spec string

--- clingo/scripts/install_clingo.py
+++ clingo/scripts/install_clingo.py
@@ -6,14 +6,10 @@ import spack.bootstrap.config
 import spack.main
 
 
-if sys.platform == "linux":
-    CLINGO_BASE_SPEC = "clingo-bootstrap@spack +static_libstdcpp +optimized +ipo ~docs"
-else:
-    CLINGO_BASE_SPEC = "clingo-bootstrap@spack +optimized +ipo ~docs"
+CLINGO_BASE_SPEC = "clingo-bootstrap@spack +optimized ~docs"
+
 
 install = spack.main.SpackCommand('install')
-
-
 with spack.bootstrap.config.spack_python_interpreter():
     msg = 'Installing clingo-bootstrap with Python: {0}'
     print(msg.format(spack.bootstrap.config.spec_for_current_python()))
