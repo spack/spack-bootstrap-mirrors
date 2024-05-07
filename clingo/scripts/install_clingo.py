12e2222670027f2eeade8808700277e4dee64477
Harmen Stoppels <harmenstoppels@gmail.com>
2024-05-07 12:19:58 +0200

    use __main__

--- clingo/scripts/install_clingo.py
+++ clingo/scripts/install_clingo.py
@@ -4,15 +4,18 @@ import archspec.cpu
 import spack.bootstrap.config
 import spack.main
 
-if sys.platform == "linux":
-    CLINGO_BASE_SPEC = "clingo-bootstrap@spack +static_libstdcpp +optimized +ipo ~docs"
-else:
-    CLINGO_BASE_SPEC = "clingo-bootstrap@spack +optimized +ipo ~docs"
+def main():
+    if sys.platform == "linux":
+        CLINGO_BASE_SPEC = "clingo-bootstrap@spack +static_libstdcpp +optimized +ipo ~docs"
+    else:
+        CLINGO_BASE_SPEC = "clingo-bootstrap@spack +optimized +ipo ~docs"
 
-install = spack.main.SpackCommand("install")
+    install = spack.main.SpackCommand("install")
 
+    with spack.bootstrap.config.spack_python_interpreter():
+        msg = "Installing clingo-bootstrap with Python: {0}"
+        print(msg.format(spack.bootstrap.config.spec_for_current_python()))
+        install(f"{CLINGO_BASE_SPEC} target={archspec.cpu.host().family}")
 
-with spack.bootstrap.config.spack_python_interpreter():
-    msg = "Installing clingo-bootstrap with Python: {0}"
-    print(msg.format(spack.bootstrap.config.spec_for_current_python()))
-    install(f"{CLINGO_BASE_SPEC} target={archspec.cpu.host().family}")
+if __name__ == "__main__":
+    main()
