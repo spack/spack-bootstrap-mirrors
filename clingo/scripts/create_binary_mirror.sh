d952bd40253bbb7600f41b8245697f0246d7fe97
Harmen Stoppels <me@harmenstoppels.nl>
2024-10-16 12:30:53 +0200

    simplify further

--- clingo/scripts/create_binary_mirror.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-specs=$(spack find --format="/{hash}" clingo-bootstrap)
-mkdir -p /home/spack/binary-mirror
-spack buildcache push  --unsigned --force /home/spack/binary-mirror ${specs}
