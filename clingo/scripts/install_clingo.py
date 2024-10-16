f6591b0aef1f5de2768068e50c2b4e874b396523
Harmen Stoppels <me@harmenstoppels.nl>
2024-10-16 13:56:22 +0200

    use __name__ == "__main__" idiom

--- clingo/scripts/install_clingo.py
+++ clingo/scripts/install_clingo.py
@@ -2,13 +2,14 @@ import archspec.cpu
 import spack.bootstrap.config
 import spack.main
 
-install = spack.main.SpackCommand("install")
+if __name__ == "__main__":
+    install = spack.main.SpackCommand("install")
 
-with spack.bootstrap.config.spack_python_interpreter():
-    print(
-        f"Installing clingo-bootstrap with Python: "
-        f"{spack.bootstrap.config.spec_for_current_python()}"
-    )
-    install(
-        f"clingo-bootstrap@spack +optimized ~docs target={archspec.cpu.host().family}"
-    )
+    with spack.bootstrap.config.spack_python_interpreter():
+        print(
+            f"Installing clingo-bootstrap with Python: "
+            f"{spack.bootstrap.config.spec_for_current_python()}"
+        )
+        install(
+            f"clingo-bootstrap@spack +optimized ~docs target={archspec.cpu.host().family}"
+        )
