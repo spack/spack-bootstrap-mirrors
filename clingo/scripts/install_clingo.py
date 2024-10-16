12ac561be1fdc8c91e226dcdfab10cf47d5dbdf8
Harmen Stoppels <me@harmenstoppels.nl>
2024-10-16 11:38:12 +0200

    simplify clingo spec

--- clingo/scripts/install_clingo.py
+++ clingo/scripts/install_clingo.py
@@ -1,21 +1,15 @@
-import sys
-
 import archspec.cpu
-
 import spack.bootstrap.config
 import spack.main
 
-
-if sys.platform == "linux":
-    CLINGO_BASE_SPEC = "clingo-bootstrap@spack +static_libstdcpp +optimized +ipo ~docs"
-else:
-    CLINGO_BASE_SPEC = "clingo-bootstrap@spack +optimized +ipo ~docs"
-
-install = spack.main.SpackCommand('install')
-
+install = spack.main.SpackCommand("install")
 
 with spack.bootstrap.config.spack_python_interpreter():
-    msg = 'Installing clingo-bootstrap with Python: {0}'
-    print(msg.format(spack.bootstrap.config.spec_for_current_python()))
-    clingo_str = f'{CLINGO_BASE_SPEC} target={str(archspec.cpu.host().family)}'
+    print(
+        f"Installing clingo-bootstrap with Python: "
+        f"{spack.bootstrap.config.spec_for_current_python()}"
+    )
+    clingo_str = (
+        f"clingo-bootstrap@spack +optimized ~docs target={archspec.cpu.host().family}"
+    )
     install(clingo_str)
