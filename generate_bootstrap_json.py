3ce5fbb643bb21b4d6a25bf14cf54e5e136fc5a2
Alec Scott <scott112@llnl.gov>
2025-10-30 14:04:00 -0700

    Fix unique paths for gnupg cache

--- generate_bootstrap_json.py
+++ generate_bootstrap_json.py
@@ -34,7 +34,7 @@ def run(pkg: str, deps=dt.NONE, python: bool = False):
     name = "clingo-bootstrap" if pkg == "clingo" else pkg
     shas = {
         tarball_hash(tarball): sha256(tarball)
-        for tarball in glob.glob("./clingo_binary_mirror*/**/*.spack", recursive=True)
+        for tarball in glob.glob(f"./{pkg}_binary_mirror*/**/*.spack", recursive=True)
     }
 
     specs = [
